576d2376e1afe51a727e390a028fcc427f90ab03
mercedeszafeher <mercedesz.a.feher@gmail.com>
2024-11-07 16:57:15 +0100

    starting with logout

---
+++ app/(auth)/logout/action.ts
@@ -0,0 +1,5 @@
+export async function logout() {
+  // 1. Get the session token from the cookie
+  // 2. Delete the session from the database based on the token
+  // 3. Delete the session cookie from the browser
+}
