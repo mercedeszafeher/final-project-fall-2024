3425024e63e041390ebb7aaec5e26f991cb4f0db
mercedeszafeher <mercedesz.a.feher@gmail.com>
2024-11-08 10:36:05 +0100

    .

--- app/(auth)/logout/action.ts
+++ app/(auth)/logout/action.ts
@@ -1,5 +1,11 @@
+import { cookies } from 'next/headers';
+
 export async function logout() {
   // 1. Get the session token from the cookie
+
+  const cookieStore = await cookies();
+
+  const token = cookieStore.get('sessionToken');
   // 2. Delete the session from the database based on the token
   // 3. Delete the session cookie from the browser
 }
